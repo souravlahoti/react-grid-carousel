226386d2c2c921c61b870bdbaa33eda4e08307e5
Hariom Sharma <hariom.s2510@gmail.com>
2022-05-05 17:21:39 +0400

    fix button

--- src/components/ArrowButton.tsx
+++ src/components/ArrowButton.tsx
@@ -8,22 +8,22 @@ export type ArrowButtonProps = HTMLAttributes<HTMLDivElement> & {
 
 const Button = styled.span<Pick<ArrowButtonProps, 'type'>>`
   position: absolute;
-  top: calc(50% - 14px);
-  height: 28px;
-  width: 28px;
-  background: #fff;
-  border-radius: 50%;
+  top: calc(50% - 12px);
+  height: 24px;
+  width: 24px;
+  background: #f7f7f7;
+  border-radius: 4px;
+  border: 1px solid #ccc;
   box-shadow: rgba(99, 99, 99, 0.2) 0 2px 8px 0;
   z-index: 10;
   cursor: pointer;
   font-size: 10px;
-  opacity: 0.6;
   transition: opacity 0.25s;
   left: ${({type}) => (type === 'prev' ? '-14px' : 'initial')};
   right: ${({type}) => (type === 'next' ? '-14px' : 'initial')};
 
   &:hover {
-    opacity: 1;
+    background: #fff;
   }
 
   &::before {
@@ -31,8 +31,8 @@ const Button = styled.span<Pick<ArrowButtonProps, 'type'>>`
     height: 8px;
     width: 8px;
     background: transparent;
-    border-top: 2px solid #777;
-    border-right: 2px solid #777;
+    border-top: 1.5px solid #777;
+    border-right: 1.5px solid #777;
     display: inline-block;
     position: absolute;
     top: 50%;
